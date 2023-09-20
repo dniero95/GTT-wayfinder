badae6d22047e7bb4c202345494747bad2db5b04
Dario Niero <dario.niero@enginius.com>
2023-09-21 01:02:31 +0200

    WIP: add eslint

--- src/app/services/stops.service.ts
+++ src/app/services/stops.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StopsService {
 
-  constructor() { }
+  constructor(private http:HttpClient){}
+  
+
 }
