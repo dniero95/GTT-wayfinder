f6a9fbd52dcffa31fa25c0191cf7cbe055903732
Dario Niero <dario.niero@enginius.com>
2023-09-21 01:09:14 +0200

    WIP: generate method findAllStops

--- src/app/services/stops.service.ts
+++ src/app/services/stops.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Stop } from '../interfaces/stop';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StopsService {
 
+  url = environment.apiUrl;
+
   constructor(private http:HttpClient){}
   
+  findAllStops():Observable <Stop[]>{
+    return this.http.get<Stop[]>(`${this.url}`);
+  }
 
 }
