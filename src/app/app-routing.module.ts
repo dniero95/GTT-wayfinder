1cfe4a437321c08aaa1d46d7454352ae5fd49954
Dario Niero <dario.niero@enginius.com>
2023-09-21 00:48:00 +0200

    WIP: add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { StopsInfoComponent } from './components/stops-info/stops-info.component';
+import { SelectStopsFormComponent } from './components/select-stops-form/select-stops-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'select', component: SelectStopsFormComponent },
+  { path: 'info', component: StopsInfoComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
