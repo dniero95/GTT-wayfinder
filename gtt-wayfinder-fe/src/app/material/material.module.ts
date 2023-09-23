a5c4a8d503ee8ef9f6ea14a1da1a19a206d1ef67
Dario Niero <dario.niero@enginius.com>
2023-09-23 12:32:39 +0200

    WIP: add content for 404 page

--- gtt-wayfinder-fe/src/app/material/material.module.ts
+++ gtt-wayfinder-fe/src/app/material/material.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatSelectModule],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatSelectModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+  ],
 })
 export class MaterialModule {}
